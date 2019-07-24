169e59d5926f139cce610e8fa7147582bdae71eb
haroldhyte <me@haroldhyte.com>
2019-07-24 13:17:03 -0400

    Auto-generated commit

--- main.bundle.js
+++ main.bundle.js
@@ -760,7 +760,8 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var DataService = (function () {
     function DataService(http) {
         this.http = http;
-        this.baseUrl = 'https://allyance-invoicify.herokuapp.com/api/';
+        this.baseUrl = 'https://allyiance-invoicify.herokuapp.com/api/';
+        //private baseURL = 'https://localhost:8080/api/'
         this.found = false;
         this.options = new __WEBPACK_IMPORTED_MODULE_1__angular_http__["b" /* RequestOptions */]({ withCredentials: true });
     }

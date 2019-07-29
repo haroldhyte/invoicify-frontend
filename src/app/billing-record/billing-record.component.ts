f29dc6f8dbb8c5de76d6b5454b99da2b93736499
QZ35LV@ALLYIPOC.COM <QZ35LV@ALLYIPOC.COM>
2019-07-29 14:52:34 -0400

    added sorting feature!

--- src/app/billing-record/billing-record.component.ts
+++ src/app/billing-record/billing-record.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material';
 
+
 import { DataService } from '../data.service'
 import { DeleteConfirmComponent } from '../delete-confirm/delete-confirm.component'
 import { fadeInAnimation } from '../animations/fade-in.animation';
@@ -16,6 +17,7 @@ export class BillingRecordComponent implements OnInit {
   errorMessage: string;
   successMessage: string;
   billingRecords: any[];
+ 
 
   constructor (private dataService: DataService, public dialog: MatDialog) {}
 
@@ -24,8 +26,25 @@ export class BillingRecordComponent implements OnInit {
   getBillingRecords() {
     this.dataService.getRecords("billing-record")
       .subscribe(
-        results => this.billingRecords = results,
+        results => this.billingRecords = results.sort((a,b)=> b.status.localeCompare(a.status)),
         error =>  this.errorMessage = <any>error);
   }
 
+  sortBy(category){
+    if( category=== 'id' || category === 'total'){
+     return this.billingRecords.sort((a,b)=> b[category] - a[category])
+    } 
+    if (category === 'client'){
+      return this.billingRecords.sort((a,b)=> a.client.name.localeCompare(b.client.name))
+    }
+    if(category === 'rate'){
+      return this.billingRecords.sort((a,b)=> a.rate.localeCompare(b.rate))
+    }
+    if( category === 'createdBy'){
+      return this.billingRecords.sort((a,b)=> a.createdBy.username.localeCompare(b.createdBy.username))
+    }
+    return this.billingRecords.sort((a,b)=> b[category].localeCompare(a[category]));
+    } 
+
 }
+

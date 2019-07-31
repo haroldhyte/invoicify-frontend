41755c9da95970397e08d653700e6d09737b67c5
haroldhyte <me@haroldhyte.com>
2019-07-31 10:17:27 -0400

    added sorting to invoices, consistent table formatting

--- src/app/invoice/invoice.component.ts
+++ src/app/invoice/invoice.component.ts
@@ -11,10 +11,12 @@ import { fadeInAnimation } from '../animations/fade-in.animation';
   animations: [fadeInAnimation]
 })
 export class InvoiceComponent implements OnInit {
-
   errorMessage: string;
   successMessage: string;
   invoices: any[];
+  reverse: boolean;
+
+  companyId = -1;
 
   constructor (private dataService: DataService) {}
 
@@ -26,4 +28,40 @@ export class InvoiceComponent implements OnInit {
         results => this.invoices = results,
         error =>  this.errorMessage = <any>error);
   }
+
+  sortBy(category){
+      if(this.reverse=== false){
+        this.reverse = true;
+        if( category === 'id' || category === 'total' || category === 'lineItems' ){
+          return this.invoices.sort((a,b)=> b[category] - a[category])
+        }
+        if (category === 'client'){
+          return this.invoices.sort((a,b)=> a.client.name.localeCompare(b.client.name))
+        }
+        if(category === 'rate'){
+          return this.invoices.sort((a,b)=> a.rate.localeCompare(b.rate))
+        }
+        if( category === 'createdBy'){
+          return this.invoices.sort((a,b)=> a.createdBy.username.localeCompare(b.createdBy.username))
+        }
+        return this.invoices.sort((a,b)=> b[category].localeCompare(a[category]));
+
+      } else{
+        this.reverse= false;
+        if( category === 'id' || category === 'total' || category === 'lineItems' ){
+          return this.invoices.sort((a,b)=> a[category] - b[category])
+        }
+        if (category === 'client'){
+          return this.invoices.sort((a,b)=> b.client.name.localeCompare(a.client.name))
+        }
+        if(category === 'rate'){
+          return this.invoices.sort((a,b)=> b.rate.localeCompare(a.rate))
+        }
+        if( category === 'createdBy'){
+          return this.invoices.sort((a,b)=> b.createdBy.username.localeCompare(a.createdBy.username))
+        }
+        return this.invoices.sort((a,b)=> a[category].localeCompare(b[category]));
+      }
+    }
+
 }
